9c26c43608f1fac6b550abc9b72762abadb2b7d4
Mike Canto <70115601+MikeCanto@users.noreply.github.com>
2021-11-26 17:31:38 -0600

    Add files via upload

--- Proyeto Final/paquetePrincipal/main.java
+++ Proyeto Final/paquetePrincipal/main.java
@@ -145,6 +145,40 @@ public class main {
                     System.out.println("//////////////////////////////////////////");
                     System.out.println("                 GRAFO                    ");
                     System.out.println("//////////////////////////////////////////");
+                    int opcionB = 0;
+                    long matriz[][];
+                    System.out.print("Tamaño de la matriz: ");
+                    int tamaño = 0;
+                    tamaño = teclado.nextInt();
+                    matriz = new long[tamaño][tamaño];
+
+                    System.out.println("Introduzca la matriz de pesos \n[NOTA: Infinito = 999999999]");
+                    for (int i = 0; i<tamaño; i++){
+                        for (int j = 0; j<tamaño; j++){
+                            System.out.println("Dato para [" + (i+1) + "] [" + (j+1) + "]: ");
+                            matriz[i][j] = tecladoB.nextLong();
+
+                        }
+                    }
+
+                    do {
+                        System.out.println(" ");
+                        System.out.println("|--------------------------------------------------------------------|");
+                        System.out.println("|                            MENU GRAFO:                             |");
+                        System.out.println("|--------------------------------------------------------------------|");
+                        System.out.println("| 1.- Encontrar caminos mas cortos (Floyd-Warshall)                  |");
+                        System.out.println("| 2.- Regresar menu principal                                        |");
+                        System.out.println("|--------------------------------------------------------------------|");
+                        System.out.print("Escoja una opcion: ");
+                        opcionB = tecladoB.nextInt();
+                        switch (opcionB){
+                            case 1:
+                                CaminosMinimos ruta = new CaminosMinimos();
+                                System.out.println(ruta.algoritmoFloyd(matriz));
+                                break;
+                        }
+
+                    } while (opcionB != 2);
                     break;
                 case 8:
                     System.out.println("\nSALIENDO................................");
